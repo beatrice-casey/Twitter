f754ec17717ec107a8450186a749fc60c3144445
Beatrice Casey <bac041@bucknell.edu>
2020-06-30 17:06:51 -0400

    While composing a tweet, the user can see how many characters they have left out of 280.

--- app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/ComposeActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codepath.apps.restclienttemplate.models.Tweet;
@@ -23,11 +26,12 @@ import okhttp3.Headers;
 
 public class ComposeActivity extends AppCompatActivity {
 
-    public static final int MAX_TWEET_LENGTH = 140;
+    public static final int MAX_TWEET_LENGTH = 280;
     public static final String TAG = "ComposeActivity";
 
     EditText etCompose;
     Button btnTweet;
+    TextView tvCharLeft;
 
     TwitterClient client;
 
@@ -38,8 +42,30 @@ public class ComposeActivity extends AppCompatActivity {
 
         etCompose = findViewById(R.id.etCompose);
         btnTweet = findViewById(R.id.btnTweet);
+        tvCharLeft = findViewById(R.id.tvCharLeft);
 
         client = TwitterApp.getRestClient(this);
+
+        //add a textwatcher that updates how many haracters are left in the tweet
+
+        etCompose.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //fires right before text is changed
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //fires as text is changins
+                tvCharLeft.setText(String.valueOf(charSequence.length()));
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //fires right after text is changed
+            }
+        });
 
         //add a click listener on the button so that when it is clicked we will publish the text
         //in etCompose to the twitter api
