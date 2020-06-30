f5e8f4ad94b03c909a53c6305e488045fd628ac6
Beatrice Casey <bac041@bucknell.edu>
2020-06-30 11:49:39 -0400

    Created Adapter and linked to the rest of the code. Now the user sees their twitter feed when logged in. (user image, user name, text of tweet).

--- app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -1,11 +1,21 @@
 package com.codepath.apps.restclienttemplate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.codepath.apps.restclienttemplate.models.Tweet;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import okhttp3.Headers;
 
@@ -14,6 +24,11 @@ public class TimelineActivity extends AppCompatActivity {
     TwitterClient client;
     public static final String TAG = "TimelineActivity";
 
+    RecyclerView rvTweets;
+    List<Tweet> tweets;
+    TweetsAdapter adapter;
+
+   //@SuppressLint("WrongViewCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +36,15 @@ public class TimelineActivity extends AppCompatActivity {
 
         //returns a client, which is an instance of TwitterClient.
         client = TwitterApp.getRestClient(this);
+
+        //Find the recycler view
+        rvTweets = findViewById(R.id.rvTweets);
+        //init the list of tweets and adapter
+        tweets = new ArrayList<>();
+        adapter = new TweetsAdapter(this, tweets);
+        //recycler view setup: layout manager and the adapter
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        rvTweets.setAdapter(adapter);
         populateTimeline();
     }
 
@@ -29,6 +53,16 @@ public class TimelineActivity extends AppCompatActivity {
             @Override
             public void onSuccess(int statusCode, Headers headers, JSON json) {
                 Log.i(TAG, "onSuccess " + json.toString());
+                //using the data from the api to create a list of tweets
+                JSONArray jsonArray = json.jsonArray;
+                try {
+                    //modify existing tweets list
+                    tweets.addAll(Tweet.fromJsonArray(jsonArray));
+                    //notify adapter that data has changed
+                    adapter.notifyDataSetChanged();
+                } catch (JSONException e) {
+                    Log.e(TAG, "Json exception", e);
+                }
 
             }
 
